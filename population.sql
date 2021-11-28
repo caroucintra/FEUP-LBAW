ce8f31307a6daf330d26b62ca8e7f462b3ad46a7
up201906845 <up201906845@fe.up.pt>
2021-11-28 18:55:14 +0000

    schema and population done i think

--- population.sql
+++ population.sql
@@ -1,6 +1,10 @@
-INSERT INTO authenticated_user (id, email, username, full_name, user_password, date_of_birth, isAdmin) VALUES
-    (1,'joaopinto@mail.com','jaozinho','João Pinto','passdojoao','2001-05-18', False),
-    (2,'joanapinto123@mail.com','itsmejoana','Joana Pinto','passdajoana','1995-08-20', False);
+-----------------------------------------
+-- Inserts
+-----------------------------------------
+INSERT INTO authenticated_user (id, email, username, full_name, user_password, date_of_birth, admin_permission) VALUES
+    (1, 'joaopinto@mail.com', 'jaozinho', 'João Pinto', 'passdojoao', '2001-05-18', False),
+    (2, 'joanapinto123@mail.com', 'itsmejoana', 'Joana Pinto', 'passdajoana', '1995-08-20', False),
+    (3, 'josevicente@admin.com', 'admin1', 'José Vicente', 'admin123', '1985-05-06', True);
 
 INSERT INTO user_follow (follower_id, followed_id) VALUES
     (1, 2),
@@ -11,7 +15,44 @@ INSERT INTO auction (id, auction_name, initial_price, deadline, item_description
 
 INSERT INTO auction_follow (follower_id, auction_id) VALUES
     (1, 1);
+	
+INSERT INTO category (id, type) VALUES
+	(1, 'Clothing'),
+	(2, 'Painting');
 
-INSERT INTO category (id, category_name) VALUES
-    (1, 'Clothing'),
-    (1, 'Painting');
+INSERT INTO auction_category (auction_id, category_id) VALUES
+    (1, 1),
+    (1, 2);
+
+INSERT INTO bid (id, bid_value, auction_id, bidder_id) VALUES
+    (1, 50.00, 1, 1);
+
+INSERT INTO greatest_bid (bid_id, auction_id) VALUES
+    (1, 1);
+
+INSERT INTO user_comment (id, auction_id, comment_text) VALUES
+    (1, 1, 'I love this painting job!');
+
+INSERT INTO money_transaction (id, user_id, admin_id, transaction_value, type) VALUES
+    (1, 1, 3, 35.00, 'Deposit');
+
+INSERT INTO user_notification (id, user_id, type, follower_id) VALUES
+    (1, 2, 'New Follower', 1);
+
+INSERT INTO user_notification (id, user_id, type, comment_id) VALUES
+    (2, 2, 'New Comment', 1);
+	
+INSERT INTO item_image (address, auction_id) VALUES
+    ('xxx', 1);
+
+--SELECT * FROM authenticated_user; 
+--SELECT * FROM auction; 
+--SELECT * FROM user_follow; 
+--SELECT * FROM auction_follow; 
+--SELECT * FROM category; 
+--SELECT * FROM auction_category; 
+--SELECT * FROM bid; 
+--SELECT * FROM greatest_bid;
+--SELECT * FROM user_comment;
+--SELECT * FROM money_transaction; 
+--SELECT * FROM user_notification;
---
+++ schema.sql
@@ -0,0 +1,159 @@
+-----------------------------------------
+-- Types
+-----------------------------------------
+DROP TYPE IF EXISTS category_name CASCADE;
+DROP TYPE IF EXISTS notification_type CASCADE;
+DROP TYPE IF EXISTS transaction_type CASCADE;
+CREATE TYPE category_name AS ENUM ('Clothing', 'Painting', 'Jewelry', 'Sculpture', 'Furniture', 'Accessories', 'Other');
+CREATE TYPE notification_type AS ENUM ('New Follower', 'Auction Action', 'New Auction', 'New Bid', 'New Comment');
+CREATE TYPE transaction_type AS ENUM ('Sell', 'Buy', 'Deposit', 'Debit');
+
+-----------------------------------------
+-- Tables
+-----------------------------------------
+
+DROP TABLE IF EXISTS authenticated_user CASCADE;
+CREATE TABLE authenticated_user (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL CONSTRAINT user_email_uk UNIQUE,
+    username TEXT NOT NULL CONSTRAINT user_username_uk UNIQUE,
+    full_name TEXT NOT NULL,
+    user_password TEXT NOT NULL,
+    img TEXT,
+    date_of_birth DATE NOT NULL,        -- '2021-12-01'
+    admin_permission BOOLEAN DEFAULT FALSE,
+    credit FLOAT DEFAULT 0,
+    user_address TEXT,
+
+    CONSTRAINT minimum_age check (
+        (date_part('year',now()- date_of_birth) +
+        date_part('month',now()- date_of_birth)/12 +
+        date_part('day',now()- date_of_birth)/365
+        ) > 17)
+);
+
+DROP TABLE IF EXISTS user_follow CASCADE;
+CREATE TABLE user_follow (
+    follower_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    followed_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+
+    PRIMARY KEY (follower_id, followed_id)
+);
+
+DROP TABLE IF EXISTS auction CASCADE;
+CREATE TABLE auction (
+    id SERIAL PRIMARY KEY,
+    auction_name TEXT NOT NULL,
+    initial_price FLOAT NOT NULL,
+    deadline TIMESTAMP WITH TIME ZONE NOT NULL,         -- TIMESTAMP WITH TIME ZONE '2004-10-19 10:23:54+02'
+    item_description TEXT NOT NULL,
+    auction_owner INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE ,
+
+    CONSTRAINT valid_deadline check (
+        (date_part('month',deadline-now())*43800+
+        date_part('day',deadline-now())*1440+
+        date_part('hour',deadline-now())*60+
+        date_part('minute',deadline-now())
+        ) > 30)
+);
+
+DROP TABLE IF EXISTS auction_follow CASCADE;
+CREATE TABLE auction_follow (
+    follower_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    auction_id INTEGER NOT NULL REFERENCES auction(id) ON UPDATE CASCADE,
+
+    PRIMARY KEY (follower_id, auction_id)
+);
+
+DROP TABLE IF EXISTS category CASCADE;
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    TYPE category_name DEFAULT 'Other'
+);
+
+DROP TABLE IF EXISTS bid CASCADE;
+CREATE TABLE bid (
+    id SERIAL PRIMARY KEY,
+    bid_value FLOAT NOT NULL,
+    bid_date TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    winner BOOL DEFAULT FALSE,
+
+    auction_id INTEGER NOT NULL REFERENCES auction(id) ON UPDATE CASCADE,
+    bidder_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE
+);
+
+ DROP TABLE IF EXISTS greatest_bid CASCADE;
+ CREATE TABLE greatest_bid (
+    bid_id INTEGER NOT NULL REFERENCES bid(id) ON UPDATE CASCADE,
+    auction_id INTEGER NOT NULL REFERENCES auction(id) ON UPDATE CASCADE,
+
+    PRIMARY KEY (bid_id, auction_id)
+);
+
+DROP TABLE IF EXISTS auction_category CASCADE;
+CREATE TABLE auction_category (
+    auction_id INTEGER NOT NULL REFERENCES auction(id) ON UPDATE CASCADE,
+    category_id INTEGER NOT NULL REFERENCES category(id) ON UPDATE CASCADE,
+    
+    PRIMARY KEY (auction_id, category_id)
+);
+
+DROP TABLE IF EXISTS user_comment CASCADE;
+CREATE TABLE user_comment (
+    id SERIAL PRIMARY KEY,
+    auction_id INTEGER NOT NULL REFERENCES auction(id) ON UPDATE CASCADE,
+    comment_text TEXT NOT NULL,
+    comment_date TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
+DROP TABLE IF EXISTS money_transaction CASCADE;
+CREATE TABLE money_transaction (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    admin_id INTEGER REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    transaction_value FLOAT NOT NULL,
+    TYPE transaction_type NOT NULL,
+    auction_id INTEGER REFERENCES auction(id) ON UPDATE CASCADE,
+
+    CONSTRAINT minimum_transaction check (transaction_value > 0),
+	CONSTRAINT valid_transaction check (
+        ((type in ('Sell', 'Buy')) AND NOT(auction_id IS NULL)) OR
+        ((type in ('Deposit', 'Debit')) AND NOT(admin_id IS NULL))
+	)
+);
+
+DROP TABLE IF EXISTS item_image CASCADE;
+CREATE TABLE item_image (
+    address TEXT UNIQUE PRIMARY KEY,
+    auction_id INTEGER NOT NULL REFERENCES auction(id)
+);
+
+DROP TABLE IF EXISTS user_notification CASCADE;
+CREATE TABLE user_notification (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    type notification_type NOT NULL,
+    auction_id INTEGER REFERENCES auction(id) ON UPDATE CASCADE,
+    follower_id INTEGER REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    comment_id INTEGER REFERENCES user_comment(id) ON UPDATE CASCADE,
+    bid_id INTEGER REFERENCES bid(id),
+    
+    CONSTRAINT valid_notification check (
+        ((type IN ('New Follower')) AND NOT(follower_id IS NULL)) OR
+        ((type IN ('Auction Action')) AND NOT(auction_id IS NULL)) OR
+        ((type IN ('New Bid')) AND NOT(bid_id IS NULL)) OR
+        ((type IN ('New Comment')) AND NOT(comment_id IS NULL)) OR
+        ((type IN ('New Auction')) AND NOT(auction_id IS NULL))
+    )
+);
+
+DROP TABLE IF EXISTS review CASCADE;
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
+    id_reviewer INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    id_reviewed INTEGER NOT NULL REFERENCES authenticated_user(id) ON UPDATE CASCADE,
+    review_text TEXT DEFAULT '',
+    rating INTEGER DEFAULT 0,
+
+    CONSTRAINT valid_rating check ((rating > -1) AND (rating < 6))
+);
